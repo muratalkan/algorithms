5f126ee8b1ad634379b50598d11482d88646519f
MURAT ALKAN <43356037+MuratAlkanW@users.noreply.github.com>
2020-11-15 20:04:15 +0300

    Add files via upload

---
+++ GnomeSort.py
@@ -0,0 +1,29 @@
+import time
+import random
+import numpy as np
+
+def GnomeSort(A,n):
+    
+    ind=0
+    
+    while ind < n:
+
+        if ind == 0: 
+            ind = ind+1
+        if A[ind] >= A[ind - 1]: 
+            ind = ind+1
+        else: 
+            A[ind],A[ind-1] = A[ind-1],A[ind] 
+            ind = ind-1
+  
+    return A
+
+arr = np.random.randint(0,1000,1000)
+n = len(arr)
+
+sTime = time.time()
+arr = GnomeSort(arr, n) 
+eTime = time.time()
+
+print(arr)
+print((eTime - sTime), "seconds")
